b51180267c96cc24de8c507efd7f38dd6ce04821
farhanMustapha <38401627+farhanMustapha@users.noreply.github.com>
2022-12-13 23:28:10 +0100

    create cahier bord

---
+++ indexpmm.js
@@ -0,0 +1,25 @@
+let user={
+    password:"123",
+    userName:"admin"
+    }
+/*
+let i
+let name=['add','delete','uppdate']
+for (i=0;i<3;i++){
+    document.write("<button>"+name[i]+"<button>")
+}
+*/
+        
+ function add(){
+    document.write('<p> mission</p>')
+    document.write('<hr>')
+    document.write('<input type="text" placeholder="date">')
+    document.write('<input type="text" placeholder="activité">')
+    document.write('<input type="text" placeholder="destination">')
+    document.write('<input type="text" placeholder="chauffeur">')
+    document.write('<input type="text" placeholder="kilometrage">')
+    document.write('<hr>')
+    document.write('<button>enregistrer</button>')
+    
+}
+    
